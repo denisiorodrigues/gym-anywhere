c905c0d2543a9ca45d7e5f0e582474cf1fd87b44
Denisio Rodrigues <denisio@ymail.com>
2024-02-13 18:01:50 -0300

    feat: middleware jwt no authorized

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,12 +2,13 @@ import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
-  /** Rotas públicas */
+  /** Publics */
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  /** Rotas com autenticação */
-  app.get('/me', profile)
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
