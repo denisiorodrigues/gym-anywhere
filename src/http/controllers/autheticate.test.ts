b796f32ccc6af84d579ef88da0e4620f2fb515bb
Denisio Rodrigues <denisio@ymail.com>
2024-02-13 22:14:13 -0300

    feat: Teste E2E do autenticação

--- src/http/controllers/autheticate.test.ts
+++ src/http/controllers/autheticate.test.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe } from 'node:test'
+import { afterAll, beforeAll, expect, it } from 'vitest'
 
-test.todo('ok')
+describe('Autheticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should bo able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
