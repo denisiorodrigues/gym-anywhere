5b3637cdafb189189492a17c3947820a05f20317
Denisio Rodrigues <denisio@ymail.com>
2024-02-13 21:45:32 -0300

    feat: Configurando Test Environment do Prisma SETUP e TEARSDOWN

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,38 @@
+import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL enviroment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
-  transformMode: 'ssr',
+  transformMode: 'web',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('Teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
       },
     }
   },
