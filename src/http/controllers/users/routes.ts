208387e899d15b15e23725ad0fb29d509317067c
Denisio Rodrigues <denisio@ymail.com>
2024-02-19 07:56:30 -0300

    feat: Estratégia de refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,11 +5,14 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   /** Publics */
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
